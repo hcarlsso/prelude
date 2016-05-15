bf42b83ec116e563bced7d3a176a3fd243dea880
Hakan Carlsson <hakan.carlsson@gmail.com>
2016-05-15 22:12:24 +0200

    Started personal branch

---
+++ personal/init.el
@@ -0,0 +1,17 @@
+;; Setting up matlab-mode
+(add-to-list 'load-path "~/.emacs.d/personal/matlab-emacs")
+(load-library "matlab-load")
+(custom-set-variables
+ '(matlab-shell-command-switches '("-nodesktop -nosplash")))
+(add-hook 'matlab-mode-hook 'auto-complete-mode)
+(setq auto-mode-alist
+      (cons
+       '("\\.m$" . matlab-mode)
+       auto-mode-alist))
+
+
+
+(setq anaconda-mode-server-script "/usr/local/lib/python2.7/dist-packages/anaconda_mode.py")
+
+
+;; (provide 'init)\n;;; init.el ends here
